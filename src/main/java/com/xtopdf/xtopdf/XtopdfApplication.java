42f7741ece7294bc922d3e05104a5c5420a84c45
Joseph <JosephMillar_261@hotmail.com>
2025-01-21 10:39:38 -0700

    initial commit

---
+++ src/main/java/com/xtopdf/xtopdf/XtopdfApplication.java
@@ -0,0 +1,13 @@
+package com.xtopdf.xtopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtopdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtopdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xtopdf/xtopdf/XtopdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtopdf.xtopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtopdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
