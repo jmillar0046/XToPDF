30564b3d749215f46125e4d06c854775dbfad084
jmillar0046 <JosephMillar_261@hotmail.com>
2025-03-05 14:07:46 -0700

    Fix compile issues and update dependencies (#2)
    Tests still fail

--- src/main/java/com/xtopdf/xtopdf/controllers/FileConversionController.java
+++ src/main/java/com/xtopdf/xtopdf/controllers/FileConversionController.java
@@ -2,7 +2,6 @@ package com.xtopdf.xtopdf.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,7 @@ public class FileConversionController {
      public ResponseEntity<String> convertFile (@RequestParam("inputFile") String inputFile, @RequestParam("outputFile") String outputFile) {
         try {
             fileConversionService.convertFile(inputFile, outputFile);
-            return ResponseEntity.ok("File converted successfully")
+            return ResponseEntity.ok("File converted successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error with conversion");
         }
--- src/main/java/com/xtopdf/xtopdf/converters/DocxFileConverter.java
+++ src/main/java/com/xtopdf/xtopdf/converters/DocxFileConverter.java
@@ -1,14 +1,21 @@
 package com.xtopdf.xtopdf.converters;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.xtopdf.xtopdf.services.DocxToPdfService;
 
 @Component
 public class DocxFileConverter implements FileConverter {
 
+    @Autowired
+    private DocxToPdfService docxToPdfService;
+
     @Override
     public void convertToPDF(String inputFile, String outputFile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToPDF'");
+        docxToPdfService.convertDocxToPdf(new File(inputFile), new File(outputFile));
     }
     
 }
